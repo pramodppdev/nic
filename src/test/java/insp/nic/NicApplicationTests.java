0185e204c0c6d44d9acda3e2bd1de7b51c014739
thealchimests <thealchemists.98@gmail.com>
2023-07-03 16:54:35 +0530

    Initial commit

---
+++ src/test/java/insp/nic/NicApplicationTests.java
@@ -0,0 +1,13 @@
+package insp.nic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
