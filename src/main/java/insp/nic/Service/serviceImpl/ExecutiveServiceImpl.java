c4045c179258bc70387cb388f736caeae345cf1b
thealchimests <thealchemists.98@gmail.com>
2023-09-02 12:20:53 +0530

    Security updated for admin level

--- src/main/java/insp/nic/Service/serviceImpl/ExecutiveServiceImpl.java
+++ src/main/java/insp/nic/Service/serviceImpl/ExecutiveServiceImpl.java
@@ -7,8 +7,11 @@ import insp.nic.model.Executive;
 import insp.nic.repo.ExecutiveRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
+import javax.naming.AuthenticationException;
 import java.util.List;
+import java.util.Objects;
 
 @Service
 public class ExecutiveServiceImpl implements ExecutiveService {
@@ -35,5 +38,31 @@ public class ExecutiveServiceImpl implements ExecutiveService {
     public void deleteExe(String id) {
         executiveRepo.findById(id).orElseThrow(()->  new ResourceNotFoundException("Executive","Id",id));
         executiveRepo.deleteById(id);
+    }
+
+    @Override
+    public Executive exeAuth(Executive executiveVO) throws AuthenticationException
+    {
+
+        if (StringUtils.isEmpty(executiveVO.getExecutiveUserName()) ||StringUtils.isEmpty(executiveVO.getExecutivePasswd())) {
+            throw new AuthenticationException("Invalid user Credentials");
+        }
+
+        Executive authenticatedPOJO = executiveRepo.findAllByUserName(executiveVO.getExecutiveUserName());
+
+
+        if (authenticatedPOJO == null ) {
+            throw new AuthenticationException("User Does not exists ");
+        }
+
+        //TODO: Credentials authentication logic
+        if(Objects.equals(executiveVO.getExecutiveUserName(), authenticatedPOJO.getExecutiveUserName())
+                && Objects.equals(executiveVO.getExecutivePasswd(), authenticatedPOJO.getExecutivePasswd()))
+        {
+            authenticatedPOJO.setExecutivePasswd("");
+            return authenticatedPOJO;
+        }
+
+        throw new AuthenticationException("Invalid user Credentials");
     }
 }
